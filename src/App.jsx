437c7af03dc4e9db41ec1994f5453dd04ea1d35e
rohan-20 <20rohan.sharma08@gmail.com>
2025-06-10 00:53:45 +0530

    Updated the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,76 @@
+import { useState } from "react";
+
+const images = [
+  "/Bougainvillea.webp",
+  "/Bougainvillea_2.png",
+  "/Bougainvillea_3.png",
+];
+
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const handleClick = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
   return (
-    <div>
-      <h1>kya hum hain ?</h1>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <div
+        onClick={handleClick}
+        style={{
+          width: "50vw",
+          height: "50vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          cursor: "pointer",
+          transition: "all 0.3s ease",
+          userSelect: "none",
+          WebkitUserSelect: "none",
+          MozUserSelect: "none",
+          msUserSelect: "none",
+          WebkitTapHighlightColor: "transparent",
+        }}
+      >
+        <img
+          src={images[index]}
+          alt={`Frame ${index + 1}`}
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            objectFit: "contain",
+            transition: "opacity 0.2s ease",
+            userSelect: "none",
+            WebkitUserSelect: "none",
+            MozUserSelect: "none",
+            msUserSelect: "none",
+            pointerEvents: "none",
+          }}
+        />
+      </div>
+
+      <style>
+        {`
+          @media (max-width: 768px) {
+            div[style*="width: 50vw"] {
+              width: 80vw !important;
+              height: 60vh !important;
+            }
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
